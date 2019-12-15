e2913b4e4fb8a73b53490226ce58503777534bbc
Daniela Renuncio <danielarenuncio@gmail.com>
2019-12-15 18:58:01 -0300

    manipulando o Controller

--- public/index.php
+++ public/index.php
@@ -2,4 +2,10 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-print 'Hello World';
+//substr vai pegar a string ignorando a primeira / que vem em REQUEST_URI
+$url = substr($_SERVER['REQUEST_URI'],1);
+$url = explode("/", $url);
+
+
+$controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
+print_r($controller);

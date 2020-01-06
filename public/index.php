b346926a904f2b7036ded1d2829b475c90bb3858
Daniela Renuncio <danielarenuncio@gmail.com>
2020-01-06 15:25:08 -0300

    atualizando pages

--- public/index.php
+++ public/index.php
@@ -2,10 +2,25 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Code\Controller\PageController;
+
 //substr vai pegar a string ignorando a primeira / que vem em REQUEST_URI
 $url = substr($_SERVER['REQUEST_URI'],1);
 $url = explode("/", $url);
 
 
 $controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
-print_r($controller);
+
+$action = isset($url[1]) && $url[1] ? $url[1] : 'index';
+
+//recebo o nome do controller que eu quero
+$controller = "Code\Controller\\".ucfirst($controller).'Controller';
+print 'controller default: ' . $controller;
+print '<br> Método default: ' . $action;
+
+
+$response = call_user_func_array([new $controller, $action],[]);
+print $response();
+
+//$execute = new $controller();
+//print $execute->index();
